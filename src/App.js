a11541c24b46fa77c750d24f4ae67dcdf37cea93
Eduardo Pereira <edu.pereira3355@gmail.com>
2023-03-19 02:44:27 -0300

    Importação do banner inicial

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
-/* import { Banner } from "./components/Banner";
-import { Skills } from "./components/Skills";
+import { Banner } from "./components/Banner";
+/*import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer"; */
@@ -12,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
