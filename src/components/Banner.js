7bfde0c09dcf42304c8fb1c266fa143b4e9efa77
Eduardo Pereira <edu.pereira3355@gmail.com>
2023-03-19 11:35:54 -0300

    Finalização do banner incial

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,40 +6,40 @@ import headerImg from "../assets/img/header-img.svg"
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const [index, setIndex] = useState(1);
+    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
     const period = 2000;
 
     useEffect(() => {
-        let ticker = setInterval(() =>{
-            tick();
-        },delta)
-
+        let ticker = setInterval(() => {
+          tick();
+        }, delta);
+    
         return () => { clearInterval(ticker) };
-    }, [text])
+      }, [text])
 
-    const tick = () => {
+      const tick = () => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length);
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
         setText(updatedText);
 
-        if(isDeleting) {
-            setDelta(prevDelta => prevDelta /2 )
-        }
-
-        if(!isDeleting && updatedText == fullText){
+        if (!isDeleting && updatedText === fullText) {
             setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
             setDelta(period);
-        }else if(isDeleting && updatedText === ''){
+          } else if (isDeleting && updatedText === '') {
             setIsDeleting(false);
             setLoopNum(loopNum + 1);
+            setIndex(1);
             setDelta(500);
-
+          } else {
+            setIndex(prevIndex => prevIndex + 1);
+          }
         }
-    }
 
     return (
         <section className="banner" id = "home">
@@ -47,11 +47,11 @@ export const Banner = () => {
             <Row className="align-items-center">
                 <Col xs={12} md={6} xl={7}>
                     <span className="tagline">Welcome to my Portfolio</span>
-                    <h1>{`Hi, I'm Eduardo`}<span className="wrap">{text}</span></h1>
-                    <p>LoremIpsum</p>
+                    <h1>{`Hi! I'm Eduardo`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     <button onClick={() => console.log('teste') }>Let's Connect <ArrowRightCircle size={25} /></button>
                 </Col>
-                <Col xs={12} md={6} xl={7}>
+                <Col xs={12} md={6} xl={5}>
                     <img src={headerImg} alt="Headder img" />
                 </Col>
             </Row>
