1c76cdf5afde0cfd6f419a8b194c4f9d9e76e065
Eduardo Pereira <edu.pereira3355@gmail.com>
2023-03-26 09:46:41 -0300

    Criação da seção de contato

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { NavBar } from './components/NavBar';
 import { Banner } from "./components/Banner";
 import { Skills } from "./components//Skills";
 import { Projects } from "./components/Projects";
-/*import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer"; */
+// import { Contact } from "./components/Contact";
+// import { Footer } from "./components/Footer";
 
 function App() {
   return (
