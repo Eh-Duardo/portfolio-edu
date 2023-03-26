65f96778ae734f2bcb4a5d46bfcb7ed34004ba5b
Eduardo Pereira <edu.pereira3355@gmail.com>
2023-03-26 08:39:19 -0300

    code indentation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,58 +4,58 @@ import { ArrowRightCircle } from "react-bootstrap-icons"
 import headerImg from "../assets/img/header-img.svg"
 
 export const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const [index, setIndex] = useState(1);
-    const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
-    const period = 550;
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const period = 550;
 
-    useEffect(() => {
-        let ticker = setInterval(() => {
-          tick();
-        }, delta);
-    
-        return () => { clearInterval(ticker) };
-      }, [text])
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
 
-      const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    return () => { clearInterval(ticker) };
+  }, [text])
 
-        setText(updatedText);
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setIndex(prevIndex => prevIndex - 1);
-            setDelta(period);
-          } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setIndex(1);
-            setDelta(300);
-          } else {
-            setIndex(prevIndex => prevIndex + 1);
-          }
-        }
+    setText(updatedText);
 
-    return (
-        <section className="banner" id = "home">
-        <Container>
-            <Row className="align-items-center">
-                <Col xs={12} md={6} xl={7}>
-                    <span className="tagline">Welcome to my Portfolio</span>
-                    <h1>{`Hi, my name is Eduardo and I'm`} <span className="txt-rotate" dataPeriod="550" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>I am a 22 year old Information Systems student and I have developed this portfolio to develop my skills and be able to organize my projects.</p>
-                    <button onClick={() => console.log('connected') }>Let's Connect <ArrowRightCircle size={25} /></button>
-                </Col>
-                <Col xs={12} md={6} xl={5}>
-                    <img src={headerImg} alt="Headder img" />
-                </Col>
-            </Row>
-        </Container>
-        </section >
-    )
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(300);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>{`Hi, my name is Eduardo and I'm`} <span className="txt-rotate" dataPeriod="550" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+            <p>I am a 22 year old Information Systems student and I have developed this portfolio to develop my skills and be able to organize my projects.</p>
+            <button onClick={() => console.log('connected')}>Let's Connect <ArrowRightCircle size={25} /></button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Headder img" />
+          </Col>
+        </Row>
+      </Container>
+    </section >
+  )
 }
